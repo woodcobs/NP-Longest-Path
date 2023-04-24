711be3967d9579c70e9a1ca5e7f2371da8c2ff49
Bradley Woodcock <BradWoodcock00@gmail.com>
2023-04-24 10:04:31 -0400

    Now runs through all possible paths

--- exact_solution/longest_path_exact.py
+++ exact_solution/longest_path_exact.py
@@ -4,17 +4,35 @@ Names: Bradley Woodcock, Dylan Roth
 Date: 10/10/2019
 
 This program will find the longest path in a graph. We will be using a Directed Weighted Graph.
-The input will be the number of vertices, and the edges themselves.
+The input will be the number of vertices, and the edges themselves. This will be a brute-force solution.
+We will find the longest path in the entire graph by finding the longest path from each vertex to every other vertex.
+This will be done by using a backtracking function that will find the longest path from a given vertex to every other vertex.
+We will use itertools to generate all possible permutations of the vertices, and then we will find the longest path from each permutation.
 
 Example Input:
+    4
+    0 1 1
+    0 2 2
+    0 3 3
 """
 import itertools
+def findLongestPath(adjlist, start):
+        longestPath = 0
+
+        # find all permutations of the vertices
+        permutations = itertools.permutations(adjlist.keys())
+
+        # find the longest path from each permutation
+        for permutation in permutations:
+             print(permutation)
+        return 0
+            
 
 def main():
 
     # Input the number of vertices
-    numVertices = map(int, input().split()) 
-    numEdges = numVertices - 1
+    numVertices, numEdges = input().split(" ")
+    numVertices, numEdges = int(numVertices), int(numEdges)
 
     adjlist = {}
     for i in range(numVertices):
@@ -22,8 +40,18 @@ def main():
     
     # input the edges
     for i in range(numEdges):
-        u, v, w = map(int, input().split())
+        u, v, w = [int(x) for x in input().split(" ")]
+        v, v, w = int(u), int(v), int(w)
         adjlist[u][v] = w
+
+    # find the longest path from each vertex to every other vertex
+    longestPath = 0
+    for i in range(numVertices):
+        longestPath = max(longestPath, findLongestPath(adjlist, i))
+    return longestPath
+    pass
+
+    
     
 
 
